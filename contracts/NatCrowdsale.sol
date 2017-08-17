7d349af4d55bf1393fe27dde5fa2dea62da91557
Subangkit <subangkit@NC-PH-0862-27.web-hosting.com>
2017-08-17 05:37:45 -0700

    Update v.2

--- contracts/NatCrowdsale.sol
+++ contracts/NatCrowdsale.sol
@@ -1,8 +1,109 @@
 pragma solidity ^0.4.11;
 
 import './NatCoin.sol';
-import 'zeppelin-solidity/contracts/crowdsale/Crowdsale.sol';
-import 'zeppelin-solidity/contracts/ownership/Ownable.sol';
+
+/**
+ * @title Crowdsale
+ * @dev Crowdsale is a base contract for managing a token crowdsale.
+ * Crowdsales have a start and end block, where investors can make
+ * token purchases and the crowdsale will assign them tokens based
+ * on a token per ETH rate. Funds collected are forwarded to a wallet
+ * as they arrive.
+ */
+contract Crowdsale {
+  using SafeMath for uint256;
+
+  // The token being sold
+  MintableToken public token;
+
+  // start and end block where investments are allowed (both inclusive)
+  uint256 public startBlock;
+  uint256 public endBlock;
+
+  // address where funds are collected
+  address public wallet;
+
+  // how many token units a buyer gets per wei
+  uint256 public rate;
+
+  // amount of raised money in wei
+  uint256 public weiRaised;
+
+  /**
+   * event for token purchase logging
+   * @param purchaser who paid for the tokens
+   * @param beneficiary who got the tokens
+   * @param value weis paid for purchase
+   * @param amount amount of tokens purchased
+   */
+  event TokenPurchase(address indexed purchaser, address indexed beneficiary, uint256 value, uint256 amount);
+
+
+  function Crowdsale(uint256 _startBlock, uint256 _endBlock, uint256 _rate, address _wallet) {
+    require(_startBlock >= block.number);
+    require(_endBlock >= _startBlock);
+    require(_rate > 0);
+    require(_wallet != 0x0);
+
+    token = createTokenContract();
+    startBlock = _startBlock;
+    endBlock = _endBlock;
+    rate = _rate;
+    wallet = _wallet;
+  }
+
+  // creates the token to be sold.
+  // override this method to have crowdsale of a specific mintable token.
+  function createTokenContract() internal returns (MintableToken) {
+    return new MintableToken();
+  }
+
+
+  // fallback function can be used to buy tokens
+  function () payable {
+    buyTokens(msg.sender);
+  }
+
+  // low level token purchase function
+  function buyTokens(address beneficiary) payable {
+    require(beneficiary != 0x0);
+    require(validPurchase());
+
+    uint256 weiAmount = msg.value;
+
+    // calculate token amount to be created
+    uint256 tokens = weiAmount.mul(rate);
+
+    // update state
+    weiRaised = weiRaised.add(weiAmount);
+
+    token.mint(beneficiary, tokens);
+    TokenPurchase(msg.sender, beneficiary, weiAmount, tokens);
+
+    forwardFunds();
+  }
+
+  // send ether to the fund collection wallet
+  // override to create custom fund forwarding mechanisms
+  function forwardFunds() internal {
+    wallet.transfer(msg.value);
+  }
+
+  // @return true if the transaction can buy tokens
+  function validPurchase() internal constant returns (bool) {
+    uint256 current = block.number;
+    bool withinPeriod = current >= startBlock && current <= endBlock;
+    bool nonZeroPurchase = msg.value != 0;
+    return withinPeriod && nonZeroPurchase;
+  }
+
+  // @return true if crowdsale event has ended
+  function hasEnded() public constant returns (bool) {
+    return block.number > endBlock;
+  }
+
+
+}
 
 
 contract NatCoinCrowdsale is Crowdsale, Ownable {
@@ -33,11 +134,11 @@ contract NatCoinCrowdsale is Crowdsale, Ownable {
   NatCoin natcoinTokenContract;
 
   function NatCoinCrowdsale(uint256 _startBlock, uint256 _endBlock, uint256 _rate, address _wallet) Crowdsale(_startBlock, _endBlock, _rate, _wallet) {
-    icoSupply =      5000000 * 10**18;
-    reserveSupply =  8000000 * 10**18;
-    paymentSupply = 11000000 * 10**18;
-    coreSupply =    10500000 * 10**18;
-    reveralSupply =   500000 * 10**18;
+    icoSupply =      5000000 * 10**17;
+    reserveSupply =  8000000 * 10**17;
+    paymentSupply = 11000000 * 10**17;
+    coreSupply =    10500000 * 10**17;
+    reveralSupply =   500000 * 10**17;
   }
 
   // creates the token to be sold.
@@ -47,25 +148,25 @@ contract NatCoinCrowdsale is Crowdsale, Ownable {
   }
 
   function claimReservedTokens(address _to, uint256 _amount) payable onlyOwner {
-    if (_amount > reserveSupply - usedReserveSupply) throw;
+    if (_amount > reserveSupply - usedReserveSupply) revert();
     token.mint(_to, _amount);
     reserveSupply += _amount;
   }
 
   function claimPaymentTokens(address _to, uint256 _amount) payable onlyOwner {
-    if (_amount > paymentSupply - usedPaymentSupply) throw;
+    if (_amount > paymentSupply - usedPaymentSupply) revert();
     token.mint(_to, _amount);
     paymentSupply += _amount;
   }
 
   function claimCoreTokens(address _to, uint256 _amount) payable onlyOwner {
-    if (_amount > coreSupply - usedCoreSupply) throw;
+    if (_amount > coreSupply - usedCoreSupply) revert();
     natcoinTokenContract.mint(_to, _amount);
     coreSupply += _amount;
   }
 
   function claimReveralTokens(address _to, uint256 _amount) payable onlyOwner {
-    if (_amount > reveralSupply - reveralCoreSupply) throw;
+    if (_amount > reveralSupply - usedReveralSupply) revert();
     natcoinTokenContract.mint(_to, _amount);
     reveralSupply += _amount;
   }
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,8 +3,9 @@ var NatCoinCrowdsale = artifacts.require("./NatCoinCrowdsale.sol")
 module.exports = function(deployer, network, accounts) {
   const startBlock = web3.eth.blockNumber + 2 // blockchain block number where the crowdsale will commence. Here I just taking the current block that the contract and setting that the crowdsale starts two block after
   const endBlock = startBlock + 300  // blockchain block number where it will end. 300 is little over an hour.
-  const rate = new web3.BigNumber(3000) // rate of ether to Nat Coin in wei 1 NatCoin = 10 Wei
-  const wallet = web3.eth.accounts[0] // the address that will hold the fund. Recommended to use a multisig one for security.
+  const rate = new web3.BigNumber(300) // rate of ether to Nat Coin in wei 1 NatCoin = 10 Wei
+  const wallet = '0x06E11F1E94e054CB85dFE3010106226a27E44B88' // the address that will hold the fund. Recommended to use a multisig one for security.
 
   deployer.deploy(NatCoinCrowdsale, startBlock, endBlock, rate, wallet)
 }
+
